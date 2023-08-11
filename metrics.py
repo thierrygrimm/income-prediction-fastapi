3df3538ae12f9cced0608a47d331c56c0c7c23ba
Thierry <thierry.grimm@hotmail.ch>
2023-08-11 02:03:15 +0200

    Create report for metrics on data slices

---
+++ metrics.py
@@ -0,0 +1,56 @@
+import logging
+import joblib
+import pandas as pd
+import numpy as np
+from model import compute_model_metrics
+from data import process_data
+
+logging.basicConfig(filename="slice_output.txt", level=logging.INFO, format="%(message)s")
+logger = logging.getLogger()
+
+# Load in the data
+#logger.info("Loading the dataset")
+test = pd.read_csv("./data/test.csv")
+
+# Load in the OneHotEncoder, LabelBinarizer and Model
+encoder = joblib.load("model/OHE.pkl")
+lb = joblib.load('model/LB.pkl')
+model = joblib.load("model/rfc_model.pkl")
+
+# Categorical Features
+cat_features = [
+    "workclass",
+    "education",
+    "marital-status",
+    "occupation",
+    "relationship",
+    "race",
+    "sex",
+    "native-country",
+]
+
+# Metrics
+# logger.info("Processing the test data")
+
+# Process the test data with the process_data function.
+X_test, y_test, encoder, lb = process_data(
+    test, categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb
+)
+
+for cat in ["workclass", "race", "sex"]:
+    logger.info(f"Category {cat}:")
+    logger.info("\n")
+    for val in test[cat].unique():
+        #print(test[test[cat] == val])
+        X_slice, y_slice, encoder, lb = process_data(
+            test[test[cat] == val], categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb
+        )
+        #print(np.shape(y_slice))
+        #print(np.shape(X_slice))
+
+        precision, recall, fbeta, _ = compute_model_metrics(y_slice, model.predict(X_slice))
+        logger.info(f"{val}")
+        logger.info(f"Precision: {precision.round(3)}, Recall: {recall.round(3)}, fbeta: {fbeta.round(3)}")
+        logger.info("\n")
+    logger.info("_____________________________________________________________")
+    logger.info("\n")
